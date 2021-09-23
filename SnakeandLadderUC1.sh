74b616a43c846a13bb0f64d1d8fd8783d2942d61
asomwanshi37 <asomwanshi37@gmail.co.com>
2021-09-23 17:42:52 +0530

    Snake and Ladder game played with single player at start position 0

---
+++ SnakeandLadderUC1.sh
@@ -0,0 +1,17 @@
+using System;
+
+namespace Snakeand_LadderUC1
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int
+            PlayerPosition = 0;
+            Console.WriteLine("Game Begins!");
+            Console.WriteLine("We are in 0 position");
+           
+        }
+    }
+}
